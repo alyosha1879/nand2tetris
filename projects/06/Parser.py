2cf3abdaa1f8344206627060ffddb3120fba2fc9
Okano Yuu <alexei.fyodorovich.karamazov@gmail.com>
2018-03-13 23:50:54 +0900

    update Parser.py

--- projects/06/Parser.py
+++ projects/06/Parser.py
@@ -3,27 +3,60 @@ class Parser:
     def __init__(self):
 
         with open('Add.asm','r') as f:
-	    lines = f.readlines()	  
-            commands = []
+	    self.linesList = f.readlines()	  
+            self.commandsList = []
+            self.currentCommand = ''
 
-	    for line in lines:
+	    for line in self.linesList:
 	        if line[0:2] == '//':
 		    pass
 		elif line == '\r\n':
 	            pass
-	        else:
-	            commands.append(line)	
+                else:
+		    self.commandsList.extend(line.split())	
 
-	    print commands
+    def hasMoreCommands(self):
 
-	def hasMoreCommands(lines):
-	    if len(lines) >= 0:
-		return True
-	    else:
-		return False
+	if len(self.commandsList) > 0:
+	    return True
+        else :
+            return False
 
-			
+    def advance(self):
+
+	self.currentCommand = self.commandsList.pop(0)
+        return self.currentCommand
+
+    def commandType(self):
+
+	if self.currentCommand[0:1] == '@':
+	    return 'A_COMMAND'
+
+        # match if dest=comp or comp;jump or dest=comp;jump.
+	# only comp not match.
+	
+        if '=' in self.currentCommand:
+	    return 'C_COMMAND'
+
+        if ';' in self.currentCommand:
+	    return 'C_COMMAND'
+
+        return 'L_COMMAND'
+
+    def sysmbol(self):
+	pass			
 
 test = Parser()
+print test.commandsList
 
-	
+while True:
+    if test.hasMoreCommands():
+        test.advance()
+        print test.commandType()
+
+    else:
+        break
+
+
+#for command in test.commandsList:
+#    print command
