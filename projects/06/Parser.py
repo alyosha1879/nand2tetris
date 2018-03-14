ab8650330b768cca8a24f8b067bc2b6e83156c3e
alyosha1879 <tancok@gmail.com>
2018-03-14 11:43:51 +0900

    Update Parser.py

--- projects/06/Parser.py
+++ projects/06/Parser.py
@@ -3,48 +3,60 @@ class Parser:
     def __init__(self):
 
         with open('Add.asm','r') as f:
-	    self.linesList = f.readlines()	  
+            self.linesList = f.readlines()
             self.commandsList = []
             self.currentCommand = ''
+            self.currentType = ''
 
-	    for line in self.linesList:
-	        if line[0:2] == '//':
-		    pass
-		elif line == '\r\n':
-	            pass
+            for line in self.linesList:
+                if line[0:2] == '//':
+                    pass
+                elif line == '\r\n':
+                    pass
                 else:
-		    self.commandsList.extend(line.split())	
+                    self.commandsList.extend(line.split())
 
     def hasMoreCommands(self):
 
-	if len(self.commandsList) > 0:
-	    return True
+        if len(self.commandsList) > 0:
+            return True
         else :
             return False
 
     def advance(self):
 
-	self.currentCommand = self.commandsList.pop(0)
+        self.currentCommand = self.commandsList.pop(0)
         return self.currentCommand
 
     def commandType(self):
 
-	if self.currentCommand[0:1] == '@':
-	    return 'A_COMMAND'
+        if self.currentCommand[0:1] == '@':
+            self.currentType = 'A_COMMAND'
 
-        # match if dest=comp or comp;jump or dest=comp;jump.
-	# only comp not match.
-	
-        if '=' in self.currentCommand:
-	    return 'C_COMMAND'
+        elif self.currentCommand[0] == '(' and self.currentCommand[-1] == ')':
+            self.currentType =  'L_COMMAND'
 
-        if ';' in self.currentCommand:
-	    return 'C_COMMAND'
+        else:
+            self.currentType = 'C_COMMAND'
 
-        return 'L_COMMAND'
+        return self.currentType
 
-    def sysmbol(self):
-	pass			
+    def symbol(self):
+
+        if self.currentType == 'A_COMMAND':
+           return self.currentCommand[1:]
+
+        if self.currentType == 'L_COMMAND':
+           return self.currentCommand[1:-1]
+
+    def dest():
+        pass
+
+    def comp():
+        pass
+
+    def jump():
+        pass
 
 test = Parser()
 print test.commandsList
@@ -54,9 +66,7 @@ while True:
         test.advance()
         print test.commandType()
 
+        if test.currentType != 'C_COMMAND':
+            print test.symbol()
     else:
         break
-
-
-#for command in test.commandsList:
-#    print command
